cc6e34097aa17225c08867f2b724d9130d126de8
Foo JH <foojihaw@hotmail.com>
2024-07-14 09:23:05 -0400

    save

--- src/main.py
+++ src/main.py
@@ -1,14 +1,19 @@
 # core
 import argparse
+import json
 import logging
 import os
 import sched
+import subprocess
 import sys
 import time
 
 INTERVAL_RUN = 3
 
+logger = None
+
 def initLogger():
+  global logger
   logger = logging.getLogger(__name__)
   logging.basicConfig(stream=sys.stdout, 
     format='%(asctime)s %(filename)s:%(levelname)s - %(message)s',
@@ -21,11 +26,18 @@ def parseArgs():
   args = parser.parse_args()
 
 def doCheck():
-  logging.info ('checking...')
+  logger.info ('checking...')
+  resp = subprocess.run(['vnstat','--json'], capture_output=True)
+  try:
+    data = json.loads(resp.stdout.decode('utf-8'))
+    logger.debug(json.dumps(data, indent=2))
+  except Exception as e:
+    logger.error(e)
+
   sch.enter(INTERVAL_RUN, 1, doCheck)
 
 initLogger()
-logging.info (f'Current dir: {os.getcwd()}')
+logger.info (f'Current dir: {os.getcwd()}')
 parseArgs()
 
 sch = sched.scheduler(time.time, time.sleep)
